6a09237b5f7fa379bdb4a844adb20e613f251983
lee yoon jin <mngvmn0707@gmail.com>
2023-09-28 03:10:30 +0900

    add disney-plus first code

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3/movie/",
+  params: {
+    api_key: "bbd611fd77048d4fc541004a1f3ee850",
+    language: "ko-KR",
+  },
+});
+
+export default instance;
---
+++ src/api/requests.js
@@ -0,0 +1,12 @@
+const requests = {
+  fetchNowPlaying: "movie/now_playing",
+  fetchTrending: "/trending/all/week",
+  fetchTopRated: "/movie/top_rated",
+  fetchActionMovies: "/discover/mocie?with_genres=28",
+  fetchComedyMovies: "/discover/mocie?with_genres=35",
+  fetchHorrorMovies: "/discover/mocie?with_genres=27",
+  fetchRomanceMovies: "/discover/mocie?with_genres=10749",
+  fetchDocumentaries: "/discover/mocie?with_genres=99",
+};
+
+export default requests;
