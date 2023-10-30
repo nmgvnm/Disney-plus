f0bf67da222c0ecd15d485ad43e27f88a0cde325
lee yoon jin <mngvmn0707@gmail.com>
2023-10-30 23:09:16 +0900

    add SearchPage.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,31 +1,82 @@
 import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const Nav = () => {
   const [show, setShow] = useState(false);
+  const { pathname } = useLocation();
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 50) {
-        setShow(true);
-      } else {
-        setShow(false);
-      }
-    });
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener("scroll", () => {});
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
   return (
     <NavWrapper $show={show}>
       <Logo>
         <img alt="Disney Plus logo" src="/images/logo.svg" onClick={() => (window.location.href = "/")} />
       </Logo>
+      {pathname === "/" ? (
+        <Login>Login</Login>
+      ) : (
+        <>
+          <Input
+            value={searchValue}
+            onChange={handleChange}
+            className="nav__input"
+            type="text"
+            placeholder="영화를 검색해주세요."
+          />
+        </>
+      )}
     </NavWrapper>
   );
 };
 
 export default Nav;
+
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`;
+const Input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: rgba(0, 0, 0, 0.582);
+  border-radius: 5px;
+  color: white;
+  padding: 5px;
+  border: 1px solid #2f3442;
+`;
 
 const NavWrapper = styled.nav`
   position: fixed;
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import axios from "../../api/axios";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const SearchPage = () => {
-  return (
-    <div>
-      SearchPage
-    </div>
-  );
+  const [searchResults, setSearchResults] = useState([]);
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  let query = useQuery();
+  console.log("query:", query);
+  const searchTerm = query.get("q");
+  console.log("searchTerm:", searchTerm);
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
+      setSearchResults(request.data.results);
+      console.log("request:", request);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  return <div>SearchPage</div>;
 };
 
 export default SearchPage;
