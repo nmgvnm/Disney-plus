946c93c9e96fe51ad88752a8ea75238db61de912
lee yoon jin <mngvmn0707@gmail.com>
2023-11-12 16:48:50 +0900

    Create a custom hook that makes modals close when you click outside the modal window

--- src/MovieModal/index.jsx
+++ src/MovieModal/index.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import './MovieModal.css'
+import React, { useRef } from "react";
+import "./MovieModal.css";
+import useOnClickOutside from "../hooks/useOnClickOutside";
 
 const MovieModal = ({
   backdrop_path,
@@ -11,27 +12,28 @@ const MovieModal = ({
   vote_average,
   setModalOpen,
 }) => {
+  const ref = useRef();
+  console.log("ref: ", ref.current);
+  useOnClickOutside(ref, () => {
+    setModalOpen(false);
+  });
+  
+
   return (
     <div className="presentation" role="presentation">
       <div className="wrapper-modal">
-        <div className="modal">
-          <span 
-            onClick={() => setModalOpen(false)}
-            className="modal-close"
-          >
+        <div className="modal" ref={ref}>
+          <span onClick={() => setModalOpen(false)} className="modal-close">
             X
           </span>
-          <img 
+          <img
             className="modal__poster-img"
             src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
             alt="modal__poster-img"
           />
           <div className="modal__content">
             <p className="modal__details">
-              <span className="modal__user-perc">
-                100% for you
-              </span>{" "}
-              {release_date ? release_date : first_air_date }
+              <span className="modal__user-perc">100% for you</span> {release_date ? release_date : first_air_date}
             </p>
             <h2 className="modal__title">{title ? title : name}</h2>
             <p className="modal__overview">평점 : {vote_average}</p>
